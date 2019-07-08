ce557c47d13649bd3714ab11e7b7866814d3114d
Rishi <rishtyang@gmail.com>
2019-07-08 08:47:30 -0600

    Create run.m

---
+++ ShapeAnalysis/run.m
@@ -0,0 +1,16 @@
+load('/Users/rishi/Documents/MATLAB/ImageSequences_LacticAcid.mat');
+
+Masks = ImagingProject.ListWithMovies(51).TrackingResults.Segmentation.TimePoint(17).CellMasksInEntireZVolume;
+CurrentMask = Masks(1).ListWithPixels_3D;
+
+s1 = Shape(CurrentMask);
+s1 = CreateImage(s1);
+%ShowImage(s1)
+
+s1 = CreatePerimeter(s1);
+ShowImage(s1);
+CreateAxes(s1);
+
+
+
+
