ac3eec6fa4860243818f3edbce023ecd8c254815
Rishi <rishtyang@gmail.com>
2019-07-09 12:17:57 -0600

    Update Shape.m

--- ShapeAnalysis/Shape.m
+++ ShapeAnalysis/Shape.m
@@ -6,6 +6,9 @@ classdef Shape
         ShapePixels
         Image
         MajorAxis = [0,0,0,0];%[x1,y1,x2,y2], coordinates of farthest points on primeter
+        Area
+        Perimeter
+        
     end
     
     methods
@@ -15,18 +18,39 @@ classdef Shape
             %   Needs input of an array of n rows and 2 or more columns,
             %   in [x,y,z] format
             obj.ShapePixels = PixelListOfMask;
-            %obj.Image = [];
+            obj.Area = size(PixelListOfMask,1);
+            obj.Perimeter = [];
+            
             
         end
         function outputImage = GetImage(obj)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            if isempty(obj.Image)
-                obj = CreateImage(obj);
+            xRange = max( obj.ShapePixels(:,1) ) - min( obj.ShapePixels(:,1) ) + 1;
+            yRange = max( obj.ShapePixels(:,2) ) - min( obj.ShapePixels(:,2) ) + 1;
+            outputImage = zeros( yRange, xRange);
+            
+            for r=1:1:size(obj.ShapePixels)
+                cor = [ (obj.ShapePixels(r,1)- min(obj.ShapePixels(:,1)) +1) , (obj.ShapePixels(r,2)- min( obj.ShapePixels(:,2))+1) ];
+                outputImage(cor(2), cor(1)) = 1;
             end
             
-            outputImage = obj.Image;
-            
+        end
+        function MajorAxisToPerimeterRatio = GetMajorAxisToPerimeterRatio(obj)
+            %return number from 1 to 1.571, 1 being a circle and 1.571
+            %being a line
+            %the number could also be less than 1, and this would mean is
+            %is a really weird shape because the major axis in the longest
+            %line
+            if obj.MajorAxis == zeros(1,4)
+                obj = CreateAxes(obj);
+            end
+            if isempty(obj.Perimeter)
+                obj = CreatePerimeter(obj);
+            end
+            MajorAxisLength = sqrt( power((obj.MajorAxis(1)-obj.MajorAxis(3)),2) + power((obj.MajorAxis(2)-obj.MajorAxis(4)),2) );
+            MajorAxisToPerimeterRatio = MajorAxisLength/obj.Perimeter * pi;
+        
         end
         function ShowImage(obj)
             %METHOD1 Summary of this method goes here
@@ -46,13 +70,15 @@ classdef Shape
             
             for r=1:1:size(obj.ShapePixels)
                 cor = [ (obj.ShapePixels(r,1)- min(obj.ShapePixels(:,1)) +1) , (obj.ShapePixels(r,2)- min( obj.ShapePixels(:,2))+1) ];
-                obj.Image(cor(2), cor(1)) = 200;
+                obj.Image(cor(2), cor(1)) = 1;
             end
         
         end
         function obj = CreatePerimeter(obj)
             
             obj.Image = bwperim(obj.Image);
+            p = find(obj.Image);
+            obj.Perimeter = size(p,1);
             
         end
         function obj = CreateAxes(obj)
@@ -68,17 +94,12 @@ classdef Shape
                 PerimPoints(row,2) = ( ceil( p(row,1)/numofrows ) );
             end
             maxdist = 0;
-            tic;
             for pixel1 = 1:1: size(PerimPoints, 1)
-                disp('pixle 1');
-                disp(pixel1);
                 for pixel2 = 1:1: size(PerimPoints, 1)
-                    disp('pixle 2');
-                    disp(pixel2)
                     if pixel1~=pixel2 && PerimPoints(pixel1,3) ~= 1 && PerimPoints(pixel1,3) ~= 1
+                        
                         ydist = PerimPoints(pixel1,1) - PerimPoints(pixel2,1);
                         xdist = PerimPoints(pixel1,2) - PerimPoints(pixel2,2);
-                        
                         if sqrt((ydist*ydist)+(xdist*xdist)) > maxdist 
                             maxdist = sqrt((ydist*ydist)+(xdist*xdist));
                             obj.MajorAxis(1)=PerimPoints(pixel1,2);
@@ -90,10 +111,9 @@ classdef Shape
                 end
                 PerimPoints(pixel1,3) = 1; 
             end
-            toc;
-            
             
         end
+        
         
         
         
